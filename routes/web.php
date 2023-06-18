9aaf1fcecab3e912dc797214c42fd00d5e521fe6
majidarabi1993 <136170937+majidarabi1993@users.noreply.github.com>
2023-06-18 21:28:36 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contact', function() {
+    return 'تماس با ما';
+});
+Route::get('/home', function() {
+    return 'صفحه اصلی';
+});
+Route::get('/about', function() {
+    return 'تماس با ما';
+});
